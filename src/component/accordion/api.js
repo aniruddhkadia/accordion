3017194ec46a00529492d4f136ccdc4956a77f33
aniruddh kadia <aniruddh.kadia@gmail.com>
2022-03-29 15:30:02 +0530

    changes

---
+++ src/component/accordion/api.js
@@ -0,0 +1,32 @@
+export const questions = [
+  {
+    id: 1,
+    question: "What is the virtual DOM?",
+    answer:
+      "DOM stands for Document Object Model. The DOM represnts an HTML document with a number.",
+  },
+  {
+    id: 2,
+    question: "How do you create a react app?",
+    answer:
+      "Install  the create-react-app package using the command prompt or terminal.",
+  },
+  {
+    id: 3,
+    question: "What is an event in React?",
+    answer:
+      "An event is an action that a user or system may trigger, such as pressing a key, a mouse click, etc.",
+  },
+  {
+    id: 4,
+    question: "What are synthetic events in React?",
+    answer:
+      "Synthetic events combine the response of different browsers native events into one API, ensuring that the.",
+  },
+  {
+    id: 5,
+    question: "What are the componenets in React?",
+    answer:
+      "Components are the building blocks of any React application, and a single app usually consists of multiple components. A components is essentially a piece of the user interface. It splits the user interface into independent, reusable parts that can be processed separately.",
+  },
+];
